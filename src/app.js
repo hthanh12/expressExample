81494d68589545298bd2683e25a2c827469c9fc9
hthanh12 <nghoangthanh1212@gmail.com>
2021-01-05 18:11:50 +0700

    add babel

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const express = require("express");
+import express from 'express'
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const app = express();
